571bf8264744c1cfff5ec09143fd7db1257679d7
Benjamin Desmoulins <benjamin.desmoulins@gmail.com>
2022-09-14 16:53:33 +0200

    simplification fetch function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var best_movie = document.querySelector(".best_movie")
 var slider = document.querySelector('.slider');
 var sliders = document.querySelectorAll('.slider');
 var slider1 = document.querySelector('.slider1');
@@ -15,76 +16,58 @@ var leftBtn3 = slider3.querySelector('button.left');
 var rightBtn3 = slider3.querySelector('button.right');
 var leftBtn4 = slider4.querySelector('button.left');
 var rightBtn4 = slider4.querySelector('button.right');
+
 var value = li.length;
 
-// for (var i = 0; i < sliders.length; i++) {  
-//   sliders[i].firstElementChild.firstElementChild.style.width = li.length * 400 + 300 + 'px';
-// }
-// slider.firstElementChild.style.width = max-content;
+// FUNCTIONS FOR SCROLLING SLIDERS
 
 function scrollRightFirstSlider() {
-  console.log('right');
   var newContent = slider.firstElementChild.firstElementChild.firstElementChild.cloneNode(true);
   slider.firstElementChild.firstElementChild.appendChild(newContent);
   slider.firstElementChild.firstElementChild.removeChild(slider.firstElementChild.firstElementChild.firstElementChild);
 }
 
 function scrollLeftFirstSlider() {
-  console.log('left');
   var newContent = slider.firstElementChild.firstElementChild.lastElementChild.cloneNode(true);
   slider.firstElementChild.firstElementChild.insertBefore(newContent, slider.firstElementChild.firstElementChild.firstElementChild);
   slider.firstElementChild.firstElementChild.removeChild(slider.firstElementChild.firstElementChild.lastElementChild);
 }
 
 function scrollLeftSecondSlider() {
-  console.log('left');
   var newContent = slider2.firstElementChild.lastElementChild.cloneNode(true);
   slider2.firstElementChild.insertBefore(newContent, slider2.firstElementChild.firstElementChild);
   slider2.firstElementChild.removeChild(slider2.firstElementChild.lastElementChild);
 }
 
 function scrollRightSecondSlider() {
-  console.log('right');
   var newContent = slider2.firstElementChild.firstElementChild.cloneNode(true);
   slider2.firstElementChild.appendChild(newContent);
   slider2.firstElementChild.removeChild(slider2.firstElementChild.firstElementChild);
 }
 
 function scrollLeftThirdSlider() {
-  console.log('left');
   var newContent = slider3.firstElementChild.lastElementChild.cloneNode(true);
   slider3.firstElementChild.insertBefore(newContent, slider3.firstElementChild.firstElementChild);
   slider3.firstElementChild.removeChild(slider3.firstElementChild.lastElementChild);
 }
 
 function scrollRightThirdSlider() {
-  console.log('right');
   var newContent = slider3.firstElementChild.firstElementChild.cloneNode(true);
   slider3.firstElementChild.appendChild(newContent);
   slider3.firstElementChild.removeChild(slider3.firstElementChild.firstElementChild);
 }
 
 function scrollLeftFourthSlider() {
-  console.log('left');
   var newContent = slider4.firstElementChild.lastElementChild.cloneNode(true);
   slider4.firstElementChild.insertBefore(newContent, slider4.firstElementChild.firstElementChild);
   slider4.firstElementChild.removeChild(slider4.firstElementChild.lastElementChild);
 }
 
 function scrollRightFourthSlider() {
-  console.log('right');
   var newContent = slider4.firstElementChild.firstElementChild.cloneNode(true);
   slider4.firstElementChild.appendChild(newContent);
   slider4.firstElementChild.removeChild(slider4.firstElementChild.firstElementChild);
 }
-
-
-// for (var i = 0; i < rightBtnAll.length; i++) {
-//   rightBtnAll[i].addEventListener('click', scrollRight(i));
-// }
-// for (var i = 0; i < leftBtnAll.length; i++) {
-//   leftBtnAll[i].addEventListener('click', scrollLeft(i));
-// }
 
 rightBtn.addEventListener('click', scrollRightFirstSlider);
 leftBtn.addEventListener('click', scrollLeftFirstSlider);
@@ -95,7 +78,7 @@ leftBtn3.addEventListener('click', scrollLeftThirdSlider);
 rightBtn4.addEventListener('click', scrollRightFourthSlider);
 leftBtn4.addEventListener('click', scrollLeftFourthSlider);
 
-// console.log(rightBtnAll[1]);
+// FUNCTION TO GET ALL IMAGE FROM CUSTOM API
 
 function getBestMovie() {
   fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score")
@@ -103,94 +86,64 @@ function getBestMovie() {
     .then(data => {
       var img = document.getElementById("best_movie_picture");
       img.src = data.results[0].image_url;
-      body.best_movie.appendChild(img);
+      best_movie.appendChild(img);
     });
   }
 
 getBestMovie();
 
   function getBestMovies() {
-    fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score")
+    fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page_size=7")
       .then(response => response.json())
       .then(data => {
-        for (var i = 0; i < 5; i++) {
+        for (var i = 0; i < 7; i++) {
           li = document.createElement("li");
+          button = document.createElement("button");
           img = document.createElement("img");
           img.src = data.results[i].image_url;
-          li.appendChild(img);
+          li.appendChild(button);
+          button.appendChild(img);
           slider1.firstElementChild.appendChild(li);
           slider1.firstElementChild.removeChild(slider1.firstElementChild.firstElementChild);
         }
-    fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page=2")
-      .then(response => response.json())
-      .then(data => {
-        for (var j = 0; j < 2; j++) {
-          li = document.createElement("li");
-          img = document.createElement("img");
-          img.src = data.results[j].image_url;
-          li.appendChild(img);
-          slider1.firstElementChild.appendChild(li);
-          slider1.firstElementChild.removeChild(slider1.firstElementChild.firstElementChild);
-        }
-      });
       });
     }
 
 getBestMovies();
 
 function getBestScienceFictionMovies() {
-  fetch("http://localhost:8000/api/v1/titles/?genre=Sci-Fi&sort_by=-imdb_score")
+  fetch("http://localhost:8000/api/v1/titles/?genre=Sci-Fi&sort_by=-imdb_score&page_size=7")
     .then(response => response.json())
     .then(data => {
-      for (var i = 0; i < 5; i++) {
+      for (var i = 0; i < 7; i++) {
         li = document.createElement("li");
+        button = document.createElement("button");
         img = document.createElement("img");
         img.src = data.results[i].image_url;
-        li.appendChild(img);
+        li.appendChild(button);
+        button.appendChild(img);
         slider2.firstElementChild.appendChild(li);
         slider2.firstElementChild.removeChild(slider2.firstElementChild.firstElementChild);
       }
-  fetch("http://localhost:8000/api/v1/titles/?genre=Sci-Fi&sort_by=-imdb_score&page=2")
-    .then(response => response.json())
-    .then(data => {
-      for (var j = 0; j < 2; j++) {
-        li = document.createElement("li");
-        img = document.createElement("img");
-        img.src = data.results[j].image_url;
-        li.appendChild(img);
-        slider2.firstElementChild.appendChild(li);
-        slider2.firstElementChild.removeChild(slider2.firstElementChild.firstElementChild);
-      }
-    });
     });
   }
 
 getBestScienceFictionMovies();
 
 function getBestAdventureMovies() {
-  fetch("http://localhost:8000/api/v1/titles/?genre=Adventure&sort_by=-imdb_score")
+  fetch("http://localhost:8000/api/v1/titles/?genre=Adventure&sort_by=-imdb_score&page_size=7")
     .then(response => response.json())
     .then(data => {
-      for (var i = 0; i < 5; i++) {
+      for (var i = 0; i < 7; i++) {
         li = document.createElement("li");
+        button = document.createElement("button");
         img = document.createElement("img");
         img.src = data.results[i].image_url;
-        li.appendChild(img);
+        li.appendChild(button);
+        button.appendChild(img);
         slider3.firstElementChild.appendChild(li);
         slider3.firstElementChild.removeChild(slider3.firstElementChild.firstElementChild);
       }
-  fetch("http://localhost:8000/api/v1/titles/?genre=Adventure&sort_by=-imdb_score&page=2")
-    .then(response => response.json())
-    .then(data => {
-      for (var j = 0; j < 2; j++) {
-        li = document.createElement("li");
-        img = document.createElement("img");
-        img.src = data.results[j].image_url;
-        li.appendChild(img);
-        slider3.firstElementChild.appendChild(li);
-        slider3.firstElementChild.removeChild(slider3.firstElementChild.firstElementChild);
-      }
-    });
     });
   }
 
@@ -198,30 +151,46 @@ function getBestAdventureMovies() {
 getBestAdventureMovies();
 
 function getBestActionMovies() {
-  fetch("http://localhost:8000/api/v1/titles/?genre=Action&sort_by=-imdb_score")
+  fetch("http://localhost:8000/api/v1/titles/?genre=Action&sort_by=-imdb_score&page_size=7")
     .then(response => response.json())
     .then(data => {
-      for (var i = 0; i < 5; i++) {
+      for (var i = 0; i < 7; i++) {
         li = document.createElement("li");
+        button = document.createElement("button");
         img = document.createElement("img");
         img.src = data.results[i].image_url;
-        li.appendChild(img);
+        li.appendChild(button);
+        button.appendChild(img);
         slider4.firstElementChild.appendChild(li);
         slider4.firstElementChild.removeChild(slider4.firstElementChild.firstElementChild);
       }
-  fetch("http://localhost:8000/api/v1/titles/?genre=Action&sort_by=-imdb_score&page=2")
-    .then(response => response.json())
-    .then(data => {
-      for (var j = 0; j < 2; j++) {
-        li = document.createElement("li");
-        img = document.createElement("img");
-        img.src = data.results[j].image_url;
-        li.appendChild(img);
-        slider4.firstElementChild.appendChild(li);
-        slider4.firstElementChild.removeChild(slider4.firstElementChild.firstElementChild);
-      }
-    });
     });
   }
 
 getBestActionMovies();
+
+// // FUNCTION FOR MODALS TO OPEN AND CLOSE
+
+// console.log(buttons);
+// function log() {
+//   console.log("hello");
+// }
+
+// ---
+
+// function findButtons() {
+//     getBestMovies();
+//     getBestScienceFictionMovies();
+//     getBestAdventureMovies();
+//     getBestActionMovies();
+//   var buttons = document.querySelectorAll("button");
+//   for (var i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener("click", log);
+//   }
+// }
+
+// findButtons();
+// ---
+// for (var i = 0; i < buttons.length; i++) {
+//   buttons[i].addEventListener("click", log);
+// }
